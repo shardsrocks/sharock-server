366307e0fefcd3bb60f4823e45bd1665ac86da1a
Pine Mizune <pinemz@gmail.com>
2016-01-10 04:58:54 +0900

    feat(package): change field name

--- src/entities/results/package.cr
+++ src/entities/results/package.cr
@@ -2,14 +2,14 @@ module Sharock::Entities::Results
   class Package
     getter package
     getter package_deps
-    getter status_url
-    getter dev_status_url
+    getter status_badge_url
+    getter dev_status_badge_url
 
     def initialize(
       @package,
       @package_deps,
-      @status_url : String,
-      @dev_status_url : String
+      @status_badge_url : String,
+      @dev_status_badge_url : String
     )
     end
 
@@ -17,8 +17,8 @@ module Sharock::Entities::Results
       io.json_object do |object|
         object.field "package", @package
         object.field "package_deps", @package_deps
-        object.field "status_url", @status_url
-        object.field "dev_status_url", @dev_status_url
+        object.field "status_badge_url", @status_badge_url
+        object.field "dev_status_badge_url", @dev_status_badge_url
       end
     end
   end
