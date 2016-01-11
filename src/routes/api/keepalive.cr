1e4a4c853afda0228ab171b14cc468c36d0841f3
Pine Mizune <pinemz@gmail.com>
2016-01-12 00:52:04 +0900

    fix(keepalive): move keepalive api

--- src/routes/api/keepalive.cr
+++ src/routes/api/keepalive.cr
@@ -1,21 +1,16 @@
 require "kemal"
 
-module Sharock::Routes
+module Sharock::Routes::API
   class Keepalive
     def self.register
-      @@route = Keepalive.new.register
+      @@route = Sharock::Routes::API::Keepalive.new.register
     end
 
     protected def initialize
     end
 
     def register
-      get "/" do |env|
-        env.content_type = "text/plain"
-        "Hello Sharock"
-      end
-
-      get "/keepalive" do |env|
+      get "/api/keepalive" do |env|
         env.content_type = "text/plain"
         "OK"
       end
